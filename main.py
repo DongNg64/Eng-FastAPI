1efcb6c7a7e6af64a458c39e91dbec83e5befd13
Nguyen Hung <hungnguyenisme84@gmail.com>
2023-09-26 00:36:55 +0700

    add jwt

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from fastapi import FastAPI
 import uvicorn
+from app.routers import auth
 
-if __name__ == '__main__':
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+app = FastAPI()
+
+# Sử dụng tệp router users
+app.include_router(auth.router, prefix="/api/v1")
+
+# if __name__ == '__main__':
+#     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
