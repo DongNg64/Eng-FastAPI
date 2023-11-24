f4d1e6f932dc1850739c2ab871c531aee093dfb7
DongNguyen <dong.nguyen@boot.ai>
2023-11-24 19:18:10 +0700

    commit 24/11/2023

--- crawl_data/main.py
+++ crawl_data/main.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 options = webdriver.ChromeOptions() 
@@ -21,7 +24,23 @@ driver.find_element(By.CSS_SELECTOR, "a[href='/tests/']").click()
 driver.find_element(By.CSS_SELECTOR, "a[href='/tests/toeic/']").click()
 driver.find_element(By.CSS_SELECTOR, "a[href='/tests/4590/ets-23-toeic-test-1/']").click()
 driver.find_element(By.CSS_SELECTOR, "a[href='/tests/4590/ets-23-toeic-test-1/results/8860453/']").click()
-driver.find_element(By.CSS_SELECTOR, "a[href='/tests/4590/ets-23-toeic-test-1/results/8860453/details/']").click()
-                                        href="/tests/4590/ets-23-toeic-test-1/results/8860453/details/"
-# driver.find_element(By.CLASS_NAME, "btn btn-sm btn-sky").click()
+# element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href='/tests/4590/ets-23-toeic-test-1/results/8860453/details/']")))
+# ActionChains(driver).move_to_element(element).click()
+driver.execute_script("document.querySelector(`.lg-container > .row > .col-md-9 > .contentblock > h4 > a`).click()")
 
+# part 1
+# scroll to end
+driver.execute_script("Array.from(document.querySelectorAll('.context-image')).forEach((item,index) => { setTimeout(() => {  item.scrollIntoView()   }, 400 * index)})")
+time.sleep(5)
+# get image
+images = driver.execute_script("return Array.from(document.querySelectorAll('.lazyel.entered.loaded')).map(item => item.src)")
+# click 'Hiện transcript'
+driver.execute_script(f"return Array.from(document.querySelectorAll('.tab-content .test-questions-wrapper > .context-wrapper > .context-content.context-transcript.text-highlightable > p > a')).slice(0, {len(images)}).forEach(item=>item.click())")
+time.sleep(5)
+# get questions
+driver.execute_script("return Array.from(document.querySelectorAll('.tab-content .test-questions-wrapper > .context-wrapper > .context-content.context-transcript.text-highlightable > .collapse.show')).map(item => item.innerText)")
+# get answers
+driver.execute_script("return Array.from(document.querySelectorAll('.tab-content .test-questions-wrapper > .question-wrapper > .question-content.text-highlightable > .mt-2.text-success')).map(item => item.innerText).filter(item => !item.includes('\\n'))")
+
+# click next button
+driver.find_element(By.XPATH, "").click()
